04c5be5178838ff6c55587dd37a9b124633382de
SaiSrinivas321 <gssrinivas32194@gmail.com>
2022-01-05 11:33:20 +0530

    Combo offers

--- src/resolvers/Event.ts
+++ src/resolvers/Event.ts
@@ -19,13 +19,13 @@ import { MyContext } from "../utils/context";
 import { isRegisteredInEvent } from "../utils/isRegisteredInEvent";
 import { EventFAQ } from "../entities/EventFAQ";
 import Razorpay from "razorpay";
-import crypto from "crypto";
 import EventPay from "../entities/EventPay";
 import { UpdateEventPayInput } from "../inputs/EventPay";
 import { parse } from "json2csv";
 import { getRepository } from "typeorm";
 import { Timeline } from "../entities/Timeline";
 import dotenv from "dotenv";
+import crypto from 'crypto';
 
 dotenv.config();
 const axios = require('axios')
@@ -290,6 +290,160 @@ export class EventResolver {
     }
   }
 
+  @Authorized()
+  @Mutation(() => RegisterOutput)
+  async ComboOffer(@Arg("combo") combo: string, @Ctx() { user }: MyContext) {
+   
+    var combodetails;
+    if(combo === "AI Combo"){
+      combodetails = {
+        fee : 1300,
+        events : ['ckxentwt1000w1up7gi013e4e','ckxekc92m000c1up72t0h4h45','ckxen074m000p1up7bo0q5l17']
+        
+      }
+    }else if(combo === "Robotics Combo"){
+      combodetails = {
+        fee : 1300,
+        events : ['ckxemw5oi000o2bp77xtg9v0f','ckxf1ihnv003ccup7d2bn7a67','ckxezxg8v0030dbp7e8xq13uw']
+      }
+    }else if(combo === "Data Science Combo"){
+      combodetails = {
+        fee : 1200,
+        events : ['ckxepglch00122bp7gjk52d0i','ckxenjobj000u1up75tsph890','ckxf22vta003kcup79stiedlw']
+      }
+    }else if(combo === "Cybermatic Combo"){
+      combodetails = {
+        fee : 1200,
+        events : ['ckxexs6fl002fcup7hcrld72v','ckxey7kfz0029dbp70sqbcovb','ckxewph1e001hcup72ls1hrjx']
+      }
+    }else if(combo === "Electronic Combo"){
+      combodetails = {
+        fee : 1300,
+        events : ['ckxepp6pj00182bp7h5790jnr','ckxbr05w00004c9p74mji2rgd','ckxen40g6000q2bp7924y6qrm']
+      }
+    }else if(combo === "Management Workshops"){
+      combodetails = {
+        fee : 800,
+        events : ['ckxnilxyt000j0bp7d9gp9a7e','ckxevm6oi000gcup70lp17fa1']
+      }
+    }
+    if (!user) throw new Error("Login to Register");
+   var flag = 0 ;
+     await Promise.all(combodetails?.events.map(async (eve) => {
+        const event = await Event.findOne(eve , {relations: ["registeredUsers"]})
+        const userF = event?.registeredUsers.filter((useR) => useR.id === user.id);
+        if(userF?.length === 1) {
+          flag = 1;
+        }
+    })!)
+  if(flag){
+    throw new Error("User Already registered in one of the workshop");
+  }
+    const fee = combodetails?.fee! *100;
+      /* Create the order id */
+      let orderId: string = "";
+     
+      var options = {
+        amount: combodetails?.fee! * 100,
+        currency: "INR",
+        receipt: user.shaastraID + combo ,
+      };
+
+      await instance.orders.create(options, function (err: any, order: any) {
+        if (err) throw new Error("Order Creation failed. Please Retry");
+        orderId = order.id;
+      });
+
+      if (orderId === "")
+        throw new Error("Order Creation failed. Please Retry");
+
+      /* Store the details in database */
+      await Promise.all(combodetails?.events.map(async (eve) => {
+        const event = await Event.findOne(eve , {relations: ["registeredUsers"]})
+        await EventPay.create({
+          orderId,
+          amount: Math.round(Number(options.amount)/3) ,
+          event,
+          user,
+        }).save();
+      })!
+      )
+      return {
+        eventPay :{
+          orderId,
+          user,
+          amount : fee
+        }
+      };
+  }
+
+  @Authorized()
+  @Mutation(() => Boolean)
+  async ComboupdateEventPay(
+    @Arg("data") data: UpdateEventPayInput,
+    @Ctx() { user }: MyContext
+  ) {
+    try {
+      /* Verify the signature */
+      const body = data.orderId + "|" + data.payementId;
+      const expectedSignature = crypto
+        .createHmac("sha256", process.env.RAZORPAY_SECRET!)
+        .update(body.toString())
+        .digest("hex");
+      if (expectedSignature !== data.paymentSignature)
+        throw new Error("Invalid Payment Signature");
+
+      /* Update the details in database */
+      const eventpay = await EventPay.find({where : { orderId : data.orderId} , relations : ['event']})
+
+
+     await Promise.all(eventpay.map(async (eve) =>{
+        eve.isPaid = true,
+        eve.payementId = data.payementId,
+        eve.paymentSignature = data.paymentSignature
+        await eve.save();
+
+      })).catch(()=> {throw new Error("Update failed") })
+      
+
+      /* Update the user details in registered users */
+     await Promise.all(
+      eventpay.map(async(eve) =>{
+        const event = await Event.findOneOrFail(eve.event.id, {
+          relations: ["registeredUsers"],
+        });
+        event.registeredUsers.push(user);
+        await event.save();
+        await User.sendConfirmationMail({ name: user.name, eventname: event.name, email: user.email })
+        var reqdata = JSON.stringify({
+          "userId": user.id
+        });
+        
+        var config = {
+          method: 'post',
+          url: `http://143.110.247.75:5000/events/${event.id}/registrations`,
+          headers: { 
+            'Content-Type': 'application/json'
+          },
+          data : reqdata
+        };
+        
+        await axios(config)
+        .then(function (response : any) {
+          console.log(JSON.stringify(response.data));
+        })
+        .catch(function (error : any) {
+          console.log(error);
+        });
+              })
+     )
+     return true;
+ 
+    } catch (e) {
+      throw new Error(e);
+    }
+  }
+  
   @Query(() => GetEventsOutput)
   async getEvents(
     @Arg("filter", { nullable: true }) vertical: Vertical,
@@ -348,6 +502,10 @@ export class EventResolver {
 
     return csv
   }
+
+ 
+
+
 
   @Authorized(['ADMIN'])
   @Query(() => Number)
@@ -430,6 +588,7 @@ export class EventResolver {
 
     return null;
   }
+
 
   @FieldResolver(() => [EventFAQ])
   async faqs(@Root() { id }: Event) {
